1ac34a5c377c255613ebb4e31f5ca13921dee13e
DavShaw <119999924+DavShaw@users.noreply.github.com>
2022-12-07 02:42:36 -0500

    Create HacerRaices.java

---
+++ Java/Curso java (Básico)/Recordando Java/HacerRaices.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+
+public class HacerRaices
+{
+
+   public static void main(String[] args) {
+    
+    double a;
+    int b;
+    a = (1/2);
+    b = (-2);
+    System.out.println(a+""+b);
+
+   }
+    
+}
