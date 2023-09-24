854e870b6f06e3f92a67a5a6bc0758868db86cb8
David Torres <juandis0246@gmail.com>
2023-09-24 02:11:23 -0500

    Cosas pendientes (En el resumen.txt)

---
+++ Java/Codigo Limpio/davank/src/main/java/org/davshaw/controller/LoansController.java
@@ -0,0 +1,173 @@
+package org.davshaw.controller;
+
+import org.davshaw.model.Prestamos;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+public class LoansController
+{
+    /*
+    ! CRUD
+    ! C - Create DONE
+    ! R - Read TO DO
+    ! U - Update TO DO
+    ! D - Delete  TO DO
+
+    ? Hibernate structure
+
+    SessionFactory sessionFactory = new Configuration()
+        .configure("hibernate.cfg.xml")
+        .addAnnotatedClass(Prestamos.class)
+        .buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+
+        try
+        {
+            session.beginTransaction();
+            session.persist(usuario);
+            session.getTransaction().commit();
+            
+
+            return TORETURN;
+        }
+
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return TORETURN;
+        }
+
+        finally
+        {
+            session.close();
+            sessionFactory.close();
+        }
+    */
+
+    public static String crearPrestamo(int registroId, double monto)
+    {
+        SessionFactory sessionFactory = new Configuration()
+        .configure("hibernate.cfg.xml")
+        .addAnnotatedClass(Prestamos.class)
+        .buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+
+        try
+        {
+            //verificar que exista el registro
+            if(!(GroupLogs.existeRegistro(registroId)))
+            {
+                throw new IllegalArgumentException("No existe ningún registro con este ID.");
+            }
+
+            //Sino se lanzó la exception, entonces existe el registro (Por ende un usuario y grupo)
+            else 
+            {
+                session.beginTransaction();
+
+                Prestamos prestamo = new Prestamos(registroId, monto);
+
+                session.persist(prestamo);
+                session.getTransaction().commit();
+                
+                return "Prestamo realizado con éxito.";
+            }
+        }
+
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return "Error al crear el prestamo.";
+        }
+
+        finally
+        {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+
+    public static Boolean existePrestamo(int id)
+    {
+        SessionFactory sessionFactory = new Configuration()
+        .configure("hibernate.cfg.xml")
+        .addAnnotatedClass(Prestamos.class)
+        .buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+
+        try
+        {
+            session.beginTransaction();
+
+            String sql = "SELECT Count(*) FROM Prestamos WHERE id = :id";
+            Query<Long> query = session.createQuery(sql, Long.class);
+            query.setParameter(sql, id);
+
+            int count =  ((Number) query.uniqueResult()).intValue();
+
+            return count > 0;
+        }
+
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+
+        finally
+        {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+
+    public static Prestamos obtenerPrestamo(int id)
+    {
+        SessionFactory sessionFactory = new Configuration()
+        .configure("hibernate.cfg.xml")
+        .addAnnotatedClass(Prestamos.class)
+        .buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+
+        try
+        {
+            //Verificar que exista el prestamo
+            if(!(LoansController.existePrestamo(id)))
+            {
+                throw new IllegalArgumentException("No existe un prestamo con este id.");
+            }
+
+            //Sino se lanzó la exception, existe el prestamo
+            else
+            {
+                session.beginTransaction();
+
+                Prestamos prestamo = session.get(Prestamos.class, id);
+
+                session.getTransaction().commit();;
+
+                return prestamo;
+            }
+        }
+
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+
+        finally
+        {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+
+    
+}
