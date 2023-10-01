11a86984272afb965ed2488b2507adfcb7d6c2a9
David Torres <juandis0246@gmail.com>
2023-10-01 00:25:25 -0500

    Mucho uwu

--- Java/Codigo Limpio/Este_SI/Davank/src/main/java/org/davshaw/Controller/UsuarioControlador.java
+++ Java/Codigo Limpio/Este_SI/Davank/src/main/java/org/davshaw/Controller/UsuarioControlador.java
@@ -2,12 +2,53 @@ package org.davshaw.Controller;
 
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
+import org.davshaw.Model.derivatedentities.RegistroGrupo;
 import org.davshaw.Model.pureentities.Usuario;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class UsuarioControlador
 {
+
+    /*
+    ! CRUD
+    ! C - Create DONE
+    ! R - Read DONE
+    ! U - Update DONE
+    ! D - Delete  DONE
+
+    ? Hibernate structure
+
+    SessionFactory sessionFactory = new Configuration()
+        .configure("hibernate.cfg.xml")
+        .addAnnotatedClass(Usuario.class)
+        .buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+
+        try
+        {
+            session.beginTransaction();
+            session.persist(usuario);
+            session.getTransaction().commit();
+            
+
+            return TORETURN;
+        }
+
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return TORETURN;
+        }
+
+        finally
+        {
+            session.close();
+            sessionFactory.close();
+        }
+    */
+
     public static String crearUsuario(
     int dni,
     String primerNombre,
@@ -416,6 +457,204 @@ public class UsuarioControlador
         {
             e.printStackTrace();
             return "Error al cambiar la contraseña.";
+        }
+
+        finally
+        {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+
+    public static Integer contadorGrupos(int usuarioDni)
+    {
+        SessionFactory sessionFactory = new Configuration()
+        .configure("hibernate.cfg.xml")
+        .addAnnotatedClass(Usuario.class)
+        .buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+
+        try
+        {
+            //Verificar que el usuario exista
+            if(!(UsuarioControlador.existeUsuario(usuarioDni)))
+            {
+                throw new IllegalArgumentException("No existe un usuario con este dni.");
+            }
+
+            session.beginTransaction();
+
+            String sql = "SELECT Count(*) FROM RegistroGrupo WHERE (usuarioDni = :usuarioDni and nativo = :nativo)";
+            Query<Long> query = session.createNativeQuery(sql, Long.class);
+            query.setParameter("usuarioDni", usuarioDni);
+            query.setParameter("nativo", true);
+            session.getTransaction().commit();
+
+            int count = Integer.valueOf(query.uniqueResult().toString());
+            return count;
+        }
+
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+
+        finally
+        {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+
+    public static Boolean entrarGrupo(int usuarioDni, int grupoId)
+    {
+        SessionFactory sessionFactory = new Configuration()
+        .configure("hibernate.cfg.xml")
+        .addAnnotatedClass(Usuario.class)
+        .buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+
+        try
+        {
+            //Verificar que el grupo exista
+            if(!(GrupoControlador.existeGrupo(grupoId)))
+            {
+                throw new IllegalArgumentException("No existe un grupo con este id.");
+            }
+            
+            //Verificar que el usuario no este en mas de >= 3 grupos
+            else if(UsuarioControlador.contadorGrupos(usuarioDni) >= 3)
+            {
+                throw new IllegalArgumentException("El usuario está en el máximo de grupos permitidos.");
+            }
+            session.beginTransaction();
+
+            RegistroGrupo registro = new RegistroGrupo();
+            registro.setGrupoId(grupoId);
+            registro.setNativo(true);
+            registro.setUsuarioDni(usuarioDni);
+
+            session.persist(registro);
+
+            session.getTransaction().commit();
+
+            return true;
+        }
+
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+
+        finally
+        {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+
+    public static Boolean salirGrupo(int usuarioDni, int grupoId)
+    {
+        SessionFactory sessionFactory = new Configuration()
+        .configure("hibernate.cfg.xml")
+        .addAnnotatedClass(Usuario.class)
+        .buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+
+        try
+        {
+            //Verificar que el usuario este en al menos un grupo
+            if(UsuarioControlador.contadorGrupos(usuarioDni) <= 0)
+            {
+                throw new IllegalArgumentException("El usuario no está en ningún grupo.");
+            }
+
+            //Verificar que el grupo exista
+            else if(!(GrupoControlador.existeGrupo(grupoId)))
+            {
+                throw new IllegalArgumentException("No existe un grupo con este id.");
+            }
+
+            //Obtener ID del registro
+            Integer registroId = UsuarioControlador.obtenerRegistroId(usuarioDni, grupoId);
+
+            //Verificar que el registroId no sea null (No existe el registro)
+            if (registroId == null)
+            {
+                throw new IllegalArgumentException("No se encontró que el usuario éste en un grupo con este id.");
+            }
+
+            session.beginTransaction();
+
+            RegistroGrupo registro = RegistroGrupoControlador.obtenerRegistro(registroId);
+            session.remove(registro);
+
+            session.getTransaction().commit();
+
+            return true;
+        }
+
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+
+        finally
+        {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+
+    public static Integer obtenerRegistroId(int usuarioDni, int grupoId)
+    {
+        SessionFactory sessionFactory = new Configuration()
+        .configure("hibernate.cfg.xml")
+        .addAnnotatedClass(Usuario.class)
+        .buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+
+        try
+        {
+            //Verificar que el grupo exista
+            if(!(GrupoControlador.existeGrupo(grupoId)))
+            {
+                throw new IllegalArgumentException("No existe un grupo con este id.");
+            }
+
+            //Verificar que el usuario exista
+            else if(!(UsuarioControlador.existeUsuario(usuarioDni)))
+            {
+                throw new IllegalArgumentException("No existe un usuario con este dni.");
+            }
+
+            session.beginTransaction();
+
+            String sql = "SELECT id FROM RegistroGrupo WHERE (GrupoId = :grupoId AND usuarioDni = :usuarioDni AND nativo =: nativo)";
+            Query<Integer> query = session.createNativeQuery(sql, Integer.class);
+            query.setParameter("usuarioDni", usuarioDni);
+            query.setParameter("grupoId", grupoId);
+            query.setParameter("nativo", true);
+
+
+            Integer id = (Integer) query.uniqueResult();
+
+            session.getTransaction().commit();
+
+            return id;
+        }
+
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
         }
 
         finally
--- Java/Codigo Limpio/Este_SI/Davank/src/main/java/org/davshaw/Davank/DavankApplication.java
+++ Java/Codigo Limpio/Este_SI/Davank/src/main/java/org/davshaw/Davank/DavankApplication.java
@@ -13,17 +13,8 @@ public class DavankApplication
 	public static void main(String[] args)
 	{
 		SpringApplication.run(DavankApplication.class, args);
-		
-		//UsuarioControlador.crearUsuario(123, "Juan", "David", "Carrilo", "Torres", "Kibaw3f");
-		//UsuarioControlador.crearUsuario(456, "Santiago", "", "Carrillo", "Torres", "Kasf3wfSA");
-		//DepositoCuentaControlador.hacerDeposito(123, 1650);
-		//DepositoCuentaControlador.hacerDeposito(456, 520);
-		//RetiroCuentaControlador.hacerRetiro(123, 500);
-		//RetiroCuentaControlador.eliminarRetiro(1);
-		//System.out.println("asd");
-		//CuentaControlador.eliminarCuenta(5125);
-
 		System.out.println(Color.color("RED","El servidor local del proyecto Spring Boot está encendido. Todo lo que veas depués de este mensaje son los request"));
-	
+		org.davshaw.Controller.UsuarioControlador.salirGrupo(123, 1);
+		System.out.println(org.davshaw.Controller.UsuarioControlador.contadorGrupos(123));
 	}
 }
