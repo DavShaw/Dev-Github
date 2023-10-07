4cf41f732509e19c927140cc7ef6e0f752f62f55
David Torres <juandis0246@gmail.com>
2023-10-07 02:01:45 -0500

    TeamDeposit implements RR

--- Java/Codigo Limpio/Este_SI/Davank/src/main/java/org/davshaw/Controller/TeamDepositController.java
+++ Java/Codigo Limpio/Este_SI/Davank/src/main/java/org/davshaw/Controller/TeamDepositController.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import org.davshaw.Exception.InsufficientBalanceException;
 import org.davshaw.Exception.RecordNotFoundException;
+import org.davshaw.External.RequestResult;
 import org.davshaw.Model.derivatedentities.TeamDeposit;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -12,7 +13,7 @@ import org.hibernate.query.Query;
 
 public class TeamDepositController
 {
-    public static Boolean deposit(int logId, double balance)
+    public static RequestResult<Boolean> deposit(int logId, double balance)
     {
         SessionFactory sessionFactory = new Configuration()
         .configure("hibernate.cfg.xml")
@@ -26,52 +27,43 @@ public class TeamDepositController
             //Verificar que el registro exista
             if(TeamLogController.logExist(logId))
             {
-                session.beginTransaction();
-
-                //Obtener id del grupo y dni del usuario
-                int grupoId = TeamLogController.getteamId(logId);
-                int titularDni = TeamLogController.getOwnerDni(logId);
-                
-                //Verificar que la cuenta tenga el dinero para depositar
-                if(AccountController.hasEnough(titularDni, balance))
-                {
-                    //Retirar cantidad de la cuenta
-                    AccountController.withdrawBalance(titularDni, balance);
-                    //Agregar cantidad al grupo
-                    TeamController.addBalance(grupoId, balance);
-
-                    //Crear el registro
-                    TeamDeposit deposito = new TeamDeposit();
-                    deposito.setDateTime(new Date());
-                    deposito.setBalance(balance);
-                    deposito.setLogId(logId);
-
-                    session.persist(deposito);
-
-                    session.getTransaction().commit();
-
-                    
-                    
-
-                    return true;
-                }
-                
-                else
-                {
-                    throw new InsufficientBalanceException();
-                }
-            }
-
-            else
-            {
                 throw new RecordNotFoundException();
             }
+
+            //! (La validación depende de la pasada)
+            //Verificar que la cuenta tenga el dinero para depositar
+            int grupoId = TeamLogController.getteamId(logId);
+            int titularDni = TeamLogController.getOwnerDni(logId);
+
+            if(AccountController.hasEnough(titularDni, balance).getResult())
+            {
+                throw new InsufficientBalanceException();
+            }
+            
+            session.beginTransaction();
+                
+            //Retirar cantidad de la cuenta
+            AccountController.withdrawBalance(titularDni, balance);
+            //Agregar cantidad al grupo
+            TeamController.addBalance(grupoId, balance);
+
+            //Crear el registro
+            TeamDeposit deposito = new TeamDeposit();
+            deposito.setDateTime(new Date());
+            deposito.setBalance(balance);
+            deposito.setLogId(logId);
+
+            session.persist(deposito);
+
+            session.getTransaction().commit();
+
+            return new RequestResult<Boolean>(true, null, "The deposit has been done successfully.");
         }
 
         catch (Exception e)
         {
             e.printStackTrace();
-            return false;
+            return new RequestResult<Boolean>(false, null, e.getMessage());
         }
 
         finally
@@ -81,7 +73,7 @@ public class TeamDepositController
         }
     }
 
-    public static Boolean depositExist(int id)
+    public static RequestResult<Boolean> depositExist(int id)
     {
         SessionFactory sessionFactory = new Configuration()
         .configure("hibernate.cfg.xml")
@@ -97,13 +89,21 @@ public class TeamDepositController
             query.setParameter("id", id);
             int count = ((Number) query.uniqueResult()).intValue();
 
-            return count > 0;
+            if (count > 0)
+            {
+                return new RequestResult<Boolean>(true, true, "Deposit found.");
+            }
+
+            else
+            {
+                return new RequestResult<Boolean>(true, false, new RecordNotFoundException().getMessage());
+            }
         }
 
         catch (Exception e)
         {
             e.printStackTrace();
-            return false;
+            return new RequestResult<Boolean>(false, false, e.getMessage());
         }
 
         finally
@@ -113,7 +113,7 @@ public class TeamDepositController
         }
     }
 
-    public static TeamDeposit getDeposit(int id)
+    public static RequestResult<TeamDeposit> getDeposit(int id)
     {
         SessionFactory sessionFactory = new Configuration()
         .configure("hibernate.cfg.xml")
@@ -125,7 +125,7 @@ public class TeamDepositController
         try
         {
             //Verificar si existe el registro del deposito
-            if(!(TeamDepositController.depositExist(id)))
+            if(!(TeamDepositController.depositExist(id).getResult()))
             {
                 throw new RecordNotFoundException();
             }
@@ -133,13 +133,14 @@ public class TeamDepositController
 
             TeamDeposit deposito = session.get(TeamDeposit.class, id);
             session.getTransaction().commit();
-            return deposito;
+
+            return new RequestResult<TeamDeposit>(true, deposito, "Deposit found.");
         }
 
         catch (Exception e)
         {
             e.printStackTrace();
-            return null;
+            return new RequestResult<TeamDeposit>(false, null, e.getMessage());
         }
 
         finally
@@ -149,7 +150,7 @@ public class TeamDepositController
         }
     } 
 
-    public static Boolean deleteDeposit(int id)
+    public static RequestResult<Boolean> deleteDeposit(int id)
     {
         SessionFactory sessionFactory = new Configuration()
         .configure("hibernate.cfg.xml")
@@ -162,23 +163,23 @@ public class TeamDepositController
         {
 
             //Verificar que exista el deposito
-            if(!(TeamDepositController.depositExist(id)))
+            if(!(TeamDepositController.depositExist(id).getResult()))
             {
                 throw new RecordNotFoundException();
             }
 
             session.beginTransaction();
-            TeamDeposit deposito = TeamDepositController.getDeposit(id);
+            TeamDeposit deposito = TeamDepositController.getDeposit(id).getResult();
             session.remove(deposito);
             session.getTransaction().commit();
 
-            return true;
+            return new RequestResult<Boolean>(true, null, "The deposit has been deleted successfully.");
         }
 
         catch (Exception e)
         {
             e.printStackTrace();
-            return false;
+            return new RequestResult<Boolean>(false, null, e.getMessage());
         }
 
         finally
@@ -188,7 +189,7 @@ public class TeamDepositController
         }
     }
 
-    public static Double totalDeposit(int logId)
+    public static RequestResult<Double> totalDeposit(int logId)
     {
         SessionFactory sessionFactory = new Configuration()
         .configure("hibernate.cfg.xml")
@@ -213,13 +214,15 @@ public class TeamDepositController
             Double total = query.uniqueResult();
             
             //Return condition ? valueIfTrue : valueIfFalse -> Conditional operator
-            return (total != null) ? total : 0.0;
+            RequestResult<Double> returnIfTrue = new RequestResult<Double>(true, total, "Deposit found.");
+            RequestResult<Double> returnIfFalse = new RequestResult<Double>(false, 0.0, "Unknown error... Plz fixme");
+            return (total != null) ? returnIfTrue : returnIfFalse;
         }
 
         catch (Exception e)
         {
             e.printStackTrace();
-            return 0.0;
+            return new RequestResult<Double>(false, null, e.getMessage());
         }
 
         finally
